60b48fb212c3f536de5e35d82eebe893ede6218a
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-08-12 12:06:39 -0500

    Refactor Persona class to include static properties and methods, and enhance instance methods

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -1,16 +1,67 @@
 class Persona {
+    static _conteo = 0;
+
+    static get conteo() {
+        return Persona._conteo + ' instancias';
+    }
+
+    static mensaje() {
+        console.log(this.nombre); // undefined, Tiene acceso a los atributos estáticos, pero no a los de instancia
+        console.log('Hola a todos, soy un método estático');
+    }
+
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin Nombre', codigo = 'Sin Código', frase = 'Sin Frase') {
         this.nombre = nombre;
-        this.codigo = nombre;
+        this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
+    }
+
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
+
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase() {
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
     }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
-console.log(spiderman);
 console.log(ironman);
+
+spiderman.quienSoy();
+spiderman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
+
+// Persona._conteo = 2;
+console.log('Conteo statico: ', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+// Se puede agregar propiedades estáticas a la clase de manera externa a la clase, pero no es recomendable
+Persona.propiedadExterna = 'Propiedad externa estatica';
+console.log(Persona.propiedadExterna);
+console.log(Persona);
+
+
+
+
