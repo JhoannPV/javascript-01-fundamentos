fc5faf260c8e7173282ffa2e54aa950a0640ca17
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-08-17 16:58:48 -0500

    Singleton en JavaScript

---
+++ assets/js/classes/singleton.js
@@ -0,0 +1,24 @@
+class Singleton {
+    static #instancia; // undefined
+    nombre = '';
+
+    constructor(nombre = '') {
+        if (!!Singleton.#instancia) {
+            return Singleton.#instancia;
+        }
+
+        Singleton.#instancia = this;
+        this.nombre = nombre;
+
+        // return this;
+    }
+
+}
+
+const instancia1 = new Singleton('Ironman');
+const instancia2 = new Singleton('Spiderman');
+const instancia3 = new Singleton('Black Panther');
+
+console.log(`Nombre de la instancia1: ${instancia1.nombre}`);
+console.log(`Nombre de la instancia2: ${instancia2.nombre}`);
+console.log(`Nombre de la instancia3: ${instancia3.nombre}`);
